7ade84ce960deea8a8d1d48e59eeea9f50c5c006
Nate Smith <me@nwlsmith.com>
2021-09-29 19:18:52 -0400

    Added SaveFromJSON and LoadFromJSON.

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,7 @@ public class Main {
 
         Recipe recipe = new Recipe();
 
-        recipe.name = "test recipe";
-        recipe.description = "description";
-        ArrayList<String> ingredients = new ArrayList<String>();
-        ingredients.add("test 1");
-        ingredients.add("test 2");
-        ingredients.add("test 3");
-        recipe.ingredients = ingredients;
-        ArrayList<String> steps = new ArrayList<String>();
-        steps.add("test steps 1");
-        steps.add("test steps 2");
-        steps.add("test steps 3");
-        recipe.steps = steps;
+        recipe.LoadFromJSON("test recipe");
 
         recipe.PrintToConsole();
     }
--- src/Recipe.java
+++ src/Recipe.java
@@ -1,8 +1,18 @@
+import java.io.IOException;
 import java.util.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 /**
  * Class containing recipe data as well as methods for saving and loading.
  * Contains a name, description, list of ingredients, and instruction steps.
+ *
+ * JSON library: SimpleJSON https://github.com/fangyidong/json-simple
  */
 
 public class Recipe {
@@ -12,6 +22,13 @@ public class Recipe {
     public ArrayList<String> ingredients;
     public ArrayList<String> steps;
 
+    private String nameKey = "Name";
+    private String descKey = "Description";
+    private String ingredKey = "Ingredients";
+    private String stepsKey = "Steps";
+
+    private static FileWriter file;
+
     public Recipe() {
         ingredients = new ArrayList<String>();
         steps = new ArrayList<String>();
@@ -19,12 +36,78 @@ public class Recipe {
 
     public void SaveToJSON()
     {
+        JSONObject jsonObject = new JSONObject();
 
+        jsonObject.put(nameKey, name);
+        jsonObject.put(descKey, description);
+
+        JSONArray ingredientsJsonArray = new JSONArray();
+        ingredientsJsonArray.addAll(ingredients);
+        jsonObject.put(ingredKey, ingredientsJsonArray);
+
+        JSONArray stepsJsonArray = new JSONArray();
+        stepsJsonArray.addAll(steps);
+        jsonObject.put(stepsKey, stepsJsonArray);
+
+        try {
+            String fileName = "savedrecipes/" + name + ".json";
+            file = new FileWriter(fileName);
+            file.write(jsonObject.toJSONString());
+            System.out.println("Successfully Copied JSON Object to File...");
+            System.out.println("JSON Object: \n" + jsonObject);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+
+            try {
+                file.flush();
+                file.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
-    public void LoadFromJSON()
+    public void LoadFromJSON(String fileName)
     {
+        JSONParser jsonParser = new JSONParser();
 
+        name = fileName;
+
+        try {
+            Object obj = jsonParser.parse(new FileReader(GetFileName()));
+
+            JSONObject jsonObject =  (JSONObject) obj;
+
+            description = (String) jsonObject.get(descKey);
+
+            JSONArray ingredientsJsonArray = (JSONArray) jsonObject.get(ingredKey);
+            ingredients.clear();
+            for (int i = 0; i < ingredientsJsonArray.size(); i++)
+            {
+                ingredients.add(ingredientsJsonArray.get(i).toString());
+            }
+
+            JSONArray stepsJsonArray = (JSONArray) jsonObject.get(stepsKey);
+            steps.clear();
+            for (int i = 0; i < stepsJsonArray.size(); i++)
+            {
+                ingredients.add(stepsJsonArray.get(i).toString());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String GetFileName()
+    {
+        return "savedrecipes/" + name + ".json";
     }
 
     public void PrintToConsole()
