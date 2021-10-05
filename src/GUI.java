63bb22c45caab4f132556469856fe4eb9c38ecef
jayliquek <jayliquek@gmail.com>
2021-10-05 16:27:18 -0400

    current stuff

--- src/GUI.java
+++ src/GUI.java
@@ -1,23 +1,60 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 
 import javax.swing.*;
+
 import java.awt.GridLayout;
 
 class gui {
     public static void main(String args[]) {
         JFrame frame = new JFrame("Recipe book");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(1334, 750);
+        frame.setSize(1152, 782);
 
         JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 10));
-        // 1 ROW 3 COLUMNS
-        // panel.setLayout(new Gridlayout(0, 1));
+
+        // panel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+        // create buttons
+        JButton create_Recipe_button = new JButton("Create a Recipe");
+        JButton view_all_button = new JButton("View All Receipes");
+        JButton search_recipe_buttåon = new JButton("Search for a Recipe");
+
+        create_Recipe_button.setSize(350, 50);
+        create_Recipe_button.setLocation(52, 143);
+
+        view_all_button.setSize(350, 50);
+        view_all_button.setLocation(400, 143);
+
+        search_recipe_button.setSize(350, 50);
+        search_recipe_button.setLocation(746, 143);
 
         frame.add(panel, BorderLayout.CENTER);
+        panel.add(create_Recipe_button);
+        panel.add(view_all_button);
+        panel.add(search_recipe_button);
 
-        JButton button = new JButton("Press");
-        frame.getContentPane().add(button); // Adds Button to content pane of frame
+        JLabel recipe_name = new JLabel("Recipe Name");
+        recipe_name.setLocation(52, 239);
+        JLabel description = new JLabel("Description");
+        description.setLocation(52, 313);
+        JLabel ingredient_list = new JLabel("Ingredient List");
+        ingredient_list.setLocation(52, 373);
+        JLabel instructions = new JLabel("Step by step instructions");
+        instructions.setLocation(52, 489);
+
+        panel.add(recipe_name);
+        panel.add(description);
+        panel.add(ingredient_list);
+        panel.add(instructions);
+
+        // 1 ROW 3 COLUMNS
+        // panel.setLayout(new GridLayout(0, 3));
+        panel.setLayout(null);
         frame.setVisible(true);
+
+        // JButton button = new JButton("Press");
+        // frame.getContentPane().add(button); // Adds Button to content pane of frame
+        // frame.setVisible(true);
     }
 }
