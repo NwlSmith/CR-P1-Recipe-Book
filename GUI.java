30a8ad691b73e4c42cfd826d952f17078a222f60
Oliver Wang <owang00@gmail.com>
2021-10-05 16:06:30 -0400

    Add files via upload
    Uploaded the class files on accident. Here are the java files

---
+++ GUI.java
@@ -0,0 +1,9 @@
+
+public class GUI {
+	public static void main(String[]args) {
+		/* 1. Retrieve recipe
+		 * 2. Can read the entire recipe, description, ingredients, instructions
+		 * 3. Can step through the instructions one at a time*/
+		
+	}
+}
