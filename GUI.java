2df436e9e2ff626f7b83fdc09678f44b8162686f
Oliver Wang <owang00@gmail.com>
2021-10-05 22:26:26 -0400

    Delete GUI.java

--- GUI.java
+++
@@ -1,9 +0,0 @@
-
-public class GUI {
-	public static void main(String[]args) {
-		/* 1. Retrieve recipe
-		 * 2. Can read the entire recipe, description, ingredients, instructions
-		 * 3. Can step through the instructions one at a time*/
-		
-	}
-}
