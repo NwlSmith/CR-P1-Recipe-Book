f85e902db57db5fbbcf5b55031ecfdb523ee3391
Oliver Wang <ow361@nyu.edu>
2021-10-05 23:59:47 -0400

    Update Runner.java

--- src/Runner.java
+++ src/Runner.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 import java.util.*;
 import java.io.File;
 
@@ -7,7 +6,7 @@ public class Runner {
 	public static void main(String[]args) {
 		setup();
 		boolean running = true;
-		Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+		Scanner scanner = new Scanner(System.in);
 		String input;
 		
 		while(running) {
@@ -25,7 +24,7 @@ public class Runner {
 			}
 			else if(Integer.parseInt(input)== 1) {
 				// Create a recipe
-				recipes.addRecipe();
+				CreateRecipe();
 			}
 			else if(Integer.parseInt(input)== 2) {
 				/* 1. Retrieve recipe
@@ -37,8 +36,8 @@ public class Runner {
 				
 			}
 			else if(Integer.parseInt(input)== 3) {
-				// Go to recipe code and search function
-				// Should be in JenBranch
+				// Search for recipe
+				RecipeSearch();
 			}
 			
 			else {
@@ -51,7 +50,7 @@ public class Runner {
 		
 	}
 	
-	public static void setup() {
+	private static void setup() {
 		// Taken from Jen main.java
     	File folder = new File("./savedrecipes/");
     	File [] fileList = folder.listFiles();
@@ -60,29 +59,66 @@ public class Runner {
     	}
     }
 	
+	private static void CreateRecipe() {
+		Scanner scanner = new Scanner(System.in);
+		String name;
+		String description;
+		String ingredient;
+		String step;
+		ArrayList<String> ingredients = new ArrayList<String>();
+		ArrayList<String> steps = new ArrayList<String>();
+		boolean running = true;
+		
+		System.out.println("Enter a name for your recipe.");
+		name = scanner.nextLine();
+		System.out.println("Enter a description for your recipe.");
+		description = scanner.nextLine();
+		while(running) {
+			System.out.println("Enter your ingredient or type next to move onto the steps.");
+			ingredient = scanner.nextLine();
+			if(ingredient.toLowerCase().equals("next")) {
+				running = false;
+			}
+			else
+				ingredients.add(ingredient);
+		}
+		while(running) {
+			System.out.println("Enter your steps one at a type or type finish to complete this recipe.");
+			step = scanner.nextLine();
+			if(step.toLowerCase().equals("finish")) {
+				running = false;
+			}
+			else
+				steps.add(step);
+		}
+		Recipe recipeInput = new Recipe(name, description, ingredients, steps);
+		recipes.addRecipe(recipeInput);
+	}
+	
 	private static void RecipeExploration() {
-		Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+		// View all of the recipes
+		Scanner scanner = new Scanner(System.in);
 		String input;
+		ArrayList<Recipe> results; 
 		boolean running = true;
 		
 		while(running) {
-			System.out.println("\nPlease pick a recipe out of the ones below or type back to go back.");
-			// Code to retrieve the recipes
-			System.out.println("1. Lorem ipsum dolor sit amet, consectetur adipiscing elit,");
-			System.out.println("2. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
-			System.out.println("3. Ut enim ad minim veniam");
-
+			System.out.println("Please select a recipe from the ones below or type back to go to the main menu.");
+			// Print all the recipes
+			for(int i = 0; i < results.size(); i++) {
+				System.out.println(results.get(i));
+			}
 			input = scanner.nextLine();
 			
 			if(input.toLowerCase().equals("back")) {
 				running = false;
 			}
 			else {
-				// For loop for finding number through the indexes of recipes
-				// If not found, say not valid input
+				results = recipes.find(input);
 				
 				
-				//Found
+				
+				
 				System.out.println("1 for entire recipe, 2 for step-by-step");
 				input = scanner.nextLine();
 				if(Integer.parseInt(input) == 1)
@@ -92,7 +128,11 @@ public class Runner {
 				else
 					System.out.println("Not a valid answer");
 			}
-			
+			System.out.println("\nPlease pick a recipe out of the ones below or type back to go back.");
+			// Code to retrieve the recipes
+			System.out.println("1. Lorem ipsum dolor sit amet, consectetur adipiscing elit,");
+			System.out.println("2. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua");
+			System.out.println("3. Ut enim ad minim veniam");
 			System.out.println("");
 		}
 	}
@@ -109,6 +149,44 @@ public class Runner {
 		
 		for(int i = 0; i < length; i++) {
 			System.out.println(i);
+		}
+	}
+	
+	private static void RecipeSearch() {
+		Scanner scanner = new Scanner(System.in);
+		String input;
+		ArrayList<Recipe> results; 
+		boolean running = true;
+		
+		while(running) {
+			System.out.println("Please type the recipe name or back to go back to the main menu.");
+			input = scanner.nextLine();
+			
+			if(input.toLowerCase().equals("back")) {
+				running = false;
+			}
+			else {
+				results = recipes.find(input);
+				
+				System.out.println("Please select a recipe from the ones below.");
+				// Print all the recipes
+				for(int i = 0; i < results.size(); i++) {
+					System.out.println(results.get(i));
+				}
+				
+				
+				System.out.println("1 for entire recipe, 2 for step-by-step");
+				input = scanner.nextLine();
+				if(Integer.parseInt(input) == 1)
+					ViewRecipe();
+				else if(Integer.parseInt(input) == 2)
+					StepByStep();
+				else
+					System.out.println("Not a valid answer");
+			}
+			System.out.println("\nPlease pick a recipe out of the ones below or type back to go back.");
+			// Code to retrieve the recipes
+			
 		}
 	}
 }
