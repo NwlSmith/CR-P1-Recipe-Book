fe539600dfa36e305dc3c8be1f9d0b6594bb79f7
jayliquek <jayliquek@gmail.com>
2021-10-06 17:33:38 -0400

    recipe creation GUI

--- src/RecipeCreationEntry.java
+++ src/RecipeCreationEntry.java
@@ -11,11 +11,17 @@ public class RecipeCreationEntry extends JPanel {
     public RecipeCreationEntry(String name, String description, ArrayList<String> ingredients,
             ArrayList<String> steps) {
         super();
-
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.steps = steps;
         SetupEntry();
     }
 
     private void SetupEntry() {
-
+        JLabel nameLabel = new JLabel("Recipe Name");
+        JLabel descriptionLabel = new JLabel("Description");
+        JLabel ingredientsLabel = new JLabel("Ingredients List");
+        JLabel instructionsLabel = new JLabel("Step-by-Step Instructions");
     }
 }
