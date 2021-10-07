f40cb4f1a6d43eb478b6296e0b01d220f5161206
jayliquek <jayliquek@gmail.com>
2021-10-07 01:57:34 -0400

    recipe format

--- src/RecipeCreateGUI.java
+++ src/RecipeCreateGUI.java
@@ -7,7 +7,7 @@ import java.awt.GridBagLayout;
 public class RecipeCreateGUI extends JPanel {
 	private RecipeBook recipes;
 
-	private JTextField recipeName;
+	private JTextArea recipeName;
 	private JTextArea description;
 	private JTextArea ingredientList;
 	private JTextArea instructions;
@@ -18,42 +18,51 @@ public class RecipeCreateGUI extends JPanel {
 
 		// JLabel jlabel = new JLabel("This is the Recipe Creation Panel");
 		// add(jlabel, BorderLayout.CENTER);
+		JButton saveRecipe = new JButton();
 
-		recipeName = new JTextField(20);
-		description = new JTextArea(5, 20);
-		ingredientList = new JTextArea(4, 20);
-		instructions = new JTextArea(8, 20);
+		recipeName = new JTextArea(1, 40);
+		description = new JTextArea(5, 40);
+		ingredientList = new JTextArea(4, 40);
+		instructions = new JTextArea(8, 40);
 
 		setLayout(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-		gbc.insets = new Insets(4, 4, 4, 4);
+		gbc.insets = new Insets(5, 5, 5, 5);
 		gbc.anchor = GridBagConstraints.WEST;
+		add(new JLabel("Recipe Name"), gbc);
+		gbc.gridy++;
+		add(new JLabel("Description"), gbc);
+		gbc.gridy++;
+		add(new JLabel("Ingredient List"), gbc);
+		gbc.gridy++;
+		add(new JLabel("Step-by-step Instructions"), gbc);
+		gbc.gridy++;
 
-		// JLabel recipeName = new JLabel("Recipe Name");
-		// JTextField recipeNameInput = new JTextField();
-		// JLabel description = new JLabel("Description");
-		// JTextField descriptionInput = new JTextField();
-		// JLabel ingredientList = new JLabel("Ingredient List");
-		// JTextField ingredientListInput = new JTextField();
-		// JLabel instructions = new JLabel("Step-by-step Instructions");
-		// JTextField instructionsInput = new JTextField();
+		gbc.gridx += 56;
+		gbc.gridy = 0;
+		gbc.anchor = GridBagConstraints.NORTHWEST;
+		add(recipeName, gbc);
+		gbc.gridy++;
+		add(description, gbc);
+		gbc.gridy++;
+		add(ingredientList, gbc);
+		gbc.gridy++;
+		add(instructions, gbc);
 
-		// recipeName.setBounds(10, 20, 80, 25);
-		// add(recipeName, BorderLayout.WEST);
-		// recipeNameInput.setBounds(100, 20, 165, 25);
-		// add(recipeNameInput, BorderLayout.EAST);
+		gbc.gridwidth = 2;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
 
-		recipeNameInput.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				populate();
-			}
-		});
+		// recipeNameInput.addActionListener(new ActionListener() {
+		// public void actionPerformed(ActionEvent e) {
+		// populate();
+		// }
+		// });
 
 	}
 
-	private void populate() {
-		int x;
-	}
+	// private void populate() {
+	// int x;
+	// }
 }
