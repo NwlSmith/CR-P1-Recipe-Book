72f9d480a94e0740f982f57004419c3e2af1f6b9
Jen-Lopez <jennlolopz@gmail.com>
2021-10-08 21:41:45 -0400

    fix more details GUI

--- src/DetailsGUI.java
+++ src/DetailsGUI.java
@@ -1,11 +1,9 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Toolkit;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.*;
 
 public class DetailsGUI {
 	
@@ -14,7 +12,6 @@ public class DetailsGUI {
 	RecipeSearchEntry fullRecipeGUI;
 	StepByStepGUI stepByStepGUI;
 	int currentIndex;
-	
 	
 	public DetailsGUI(Recipe recipe) {
 		this.recipe = recipe;
@@ -30,47 +27,58 @@ public class DetailsGUI {
 	}
 	
     public void renderInterface() {
-        
+        JPanel mainPanel = new JPanel(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gbc.gridy = 0;
+        gbc.weightx = gbc.weighty = 1;
+		gbc.insets = new Insets(45,0,0,0);
+		mainPanel.setBackground(Color.WHITE);
 
-        JPanel panel = new JPanel(new BorderLayout());
-		panel.setBackground(Color.WHITE);
+		JLabel title = new JLabel("Recipe Book");
+		JLabel team = new JLabel("JJJON", SwingConstants.RIGHT);
+		title.setFont(new Font("Roboto", Font.BOLD, 40));
+		team.setFont(new Font("Roboto", Font.BOLD, 22));
+		title.setBorder(new EmptyBorder(0,0,15,0));
+		team.setBorder(new EmptyBorder(0,0,15,0));
 
-        // create buttons
         JButton entire_button = new JButton("View Entire Recipe");
-        JButton step_by_step_button = new JButton("Follow Step-by-step Guide");
+		JButton step_by_step_button = new JButton("View Step-by-Step Instructions");
+		entire_button.setFont(new Font("Roboto", Font.PLAIN, 18));
+		step_by_step_button.setFont(new Font("Roboto", Font.PLAIN, 18));
 
-        JPanel topPanel = new JPanel(new BorderLayout());
-		topPanel.setBackground(Color.WHITE);
+		JPanel navBar = new JPanel(new GridLayout(2,2));
+		navBar.setPreferredSize(new Dimension(1050, 110));
+		navBar.setBackground(Color.WHITE);
 
-        topPanel.add(entire_button, BorderLayout.WEST);
-        topPanel.add(step_by_step_button, BorderLayout.EAST);
+		navBar.add(title);
+		navBar.add(team);
+		navBar.add(entire_button);
+		navBar.add(step_by_step_button);
 
-        frame.add(topPanel, BorderLayout.NORTH);
-        
-        frame.add(fullRecipeGUI);
-        
+		gbc.anchor = GridBagConstraints.NORTH;
+		mainPanel.add(navBar, gbc);
+		gbc.anchor = GridBagConstraints.CENTER;
+		mainPanel.add(fullRecipeGUI, gbc);
         
         entire_button.addActionListener( new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		frame.remove(stepByStepGUI);
-        		frame.add(fullRecipeGUI);
-        		frame.revalidate();
-        		frame.repaint();
+        		mainPanel.remove(stepByStepGUI);
+        		mainPanel.add(fullRecipeGUI, gbc);
+				mainPanel.revalidate();
+        		mainPanel.repaint();
         	}
         });
-        
         
         step_by_step_button.addActionListener( new ActionListener() {
         	public void actionPerformed(ActionEvent e) {	
-        		frame.remove(fullRecipeGUI);
-        		frame.add(stepByStepGUI);
-        		frame.revalidate();
-        		frame.repaint();
+        		mainPanel.remove(fullRecipeGUI);
+        		mainPanel.add(stepByStepGUI, gbc);
+        		mainPanel.revalidate();
+        		mainPanel.repaint();
         	}
         });
 
-        panel.setLayout(null);
+		frame.add(mainPanel);
         frame.setVisible(true);
-
     }
 }
