55d19728f0f645db990346baf0884b45bf46c538
Jen-Lopez <jennlolopz@gmail.com>
2021-10-09 01:38:16 -0400

    fix formatting of step-by-step

--- src/StepByStepGUI.java
+++ src/StepByStepGUI.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,31 +15,40 @@ public class StepByStepGUI extends JPanel {
         GridBagConstraints gbc = new GridBagConstraints();
         GridBagConstraints listGbc = new GridBagConstraints();
 
-        gbc.gridx = listGbc.gridx = gbc.gridy = listGbc.gridy = 0;
-        gbc.weightx = gbc.weighty = listGbc.weightx = listGbc.weighty = 1;
-        listGbc.insets = new Insets(10, 0, 10, 0);
+        gbc.gridx = gbc.gridy = listGbc.gridx = listGbc.gridy = 0;
+        gbc.weighty = listGbc.weighty = 1;
 
-        JLabel recipeName = new JLabel(recipe.name);
-        gbc.anchor = GridBagConstraints.NORTH;
+        JLabel recipeName = new JLabel("Recipe " + recipe.name);
+        recipeName.setFont(new Font("Roboto", Font.BOLD, 20));
+        gbc.anchor = GridBagConstraints.NORTHWEST;
         add(recipeName, gbc);
 
-        JPanel listContent = new JPanel();
+        JPanel listContent = new JPanel(new GridBagLayout());
+        listContent.setPreferredSize(new Dimension(1050,320));
         listContent.setBackground(Color.WHITE);
-        listContent.setLayout(new GridBagLayout());
 
         gbc.anchor = GridBagConstraints.CENTER;
         listGbc.anchor = GridBagConstraints.WEST;
         add(listContent, gbc);
 
         JButton nextButton = new JButton("Next");
-        gbc.anchor = GridBagConstraints.SOUTH;
+        nextButton.setBorderPainted(false);
+        nextButton.setOpaque(true);
+        nextButton.setBackground(Color.BLACK);
+        nextButton.setForeground(Color.WHITE);
+		nextButton.setFont(new Font("Roboto", Font.BOLD, 16));
+
+        gbc.anchor = GridBagConstraints.PAGE_END;
 
         int instructionCount = recipe.steps.size();
 
         nextButton.addActionListener( new ActionListener() { 
-            public void actionPerformed(ActionEvent e) { 
+            public void actionPerformed(ActionEvent e) {   
                 if (currInstruction < instructionCount) {
-                    listContent.add(new JLabel(String.format("%d. %s", currInstruction+1, recipe.steps.get(currInstruction))), listGbc);
+                    JLabel newLabel = new JLabel(String.format("- %s", recipe.steps.get(currInstruction)));
+                    newLabel.setPreferredSize(new Dimension(1040, 20));
+                    newLabel.setBorder(new EmptyBorder(0,10,0,0));
+                    listContent.add(newLabel, listGbc);
                     listGbc.gridy++;
                     currInstruction++;
 
@@ -57,6 +68,5 @@ public class StepByStepGUI extends JPanel {
         });
 
         add(nextButton, gbc);
-
 	}
 }
